4255f6930e8b247ad6f7958a2ca687b030cf6755
Ethan <reality534719@gmail.com>
2024-10-18 22:48:12 +0800

    Site updated: 2024-10-18 22:48:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/18/这是一篇新的博文/","2024/10/18/hello-world/"];function toRandomPost(){
+var posts=["2024/10/18/这是一篇新的博文/","2024/10/18/带上她的眼睛/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
