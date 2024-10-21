c250d2a48894aaa89d323dd353f9d833049b2e45
reality534719 <reality534719@gmail.com>
2024-10-21 15:34:24 +0800

    Site updated: 2024-10-21 15:34:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/18/日暮/","2024/10/18/最后一个问题/","2024/10/18/带上她的眼睛/"];function toRandomPost(){
+var posts=["2024/10/18/美化Hexo博客，主题安装配置/","2024/10/18/简洁高效搭建个人博客/","2024/10/18/日暮/","2024/10/18/最后一个问题/","2024/10/18/带上她的眼睛/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
